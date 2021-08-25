aa649792505c552b843cb4f274edb86f00671240
Santiago Neyra <santineyra97@gmail.com>
2021-08-25 11:53:20 -0300

    a

--- enviar.php
+++ enviar.php
@@ -6,7 +6,7 @@ $correo = $_POST['correo'];
 $mensaje = $_POST['mensaje'];
 
 // Como funciona con el server
-$header = 'From: ' . $mail . " \r\n";
+$header = 'From: ' . $direccion . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
